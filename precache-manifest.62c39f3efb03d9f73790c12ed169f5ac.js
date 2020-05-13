925a179f402a5fde5618c560f312158e39b0565f
Lucas Alves <lucas.alves@udf.org.br>
2020-05-13 17:22:17 -0300

    Updates

---
+++ precache-manifest.62c39f3efb03d9f73790c12ed169f5ac.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8958ebd58afbc8e60f1078cbb68b3736",
+    "url": "/softmarvelql/index.html"
+  },
+  {
+    "revision": "7ed995464fc477c5adda",
+    "url": "/softmarvelql/static/css/2.9b46dffb.chunk.css"
+  },
+  {
+    "revision": "7ed995464fc477c5adda",
+    "url": "/softmarvelql/static/js/2.c4f4f71f.chunk.js"
+  },
+  {
+    "revision": "55c890ee3dd1595cef392bcba01cb932",
+    "url": "/softmarvelql/static/js/2.c4f4f71f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db911da4b065a9b6a3da",
+    "url": "/softmarvelql/static/js/main.4d681ae5.chunk.js"
+  },
+  {
+    "revision": "2ce1a483e3e0d475e8bc",
+    "url": "/softmarvelql/static/js/runtime-main.3e2386c4.js"
+  },
+  {
+    "revision": "0e890504eb19af8f12d643d181e83045",
+    "url": "/softmarvelql/static/media/logo.0e890504.png"
+  },
+  {
+    "revision": "d4a2c1c40bb027fef672a07866155cb2",
+    "url": "/softmarvelql/static/media/softplanbackground.d4a2c1c4.png"
+  }
+]);
